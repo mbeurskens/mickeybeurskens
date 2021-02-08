8d166ce32e056dbdd82b1e5b0964808123c331b8
GnomeBlue <mickeybeurskens@gmail.com>
2021-02-08 22:53:28 +0100

    Clean up javascript and add flexbox in recent post section

--- js/main.js
+++ js/main.js
@@ -17,22 +17,54 @@ function getNewSummary() {
   return div;
 }
 
+function getNewRecentPostTextBox() {
+  /**
+   *  Get a new text box for recent posts.
+   *
+   *  @return div containing text box.
+   */
+  let textBox = document.createElement("div");
+  textBox.className = "recent-post-box-text";
+
+  // Text box title, subtitle and getNewSummary
+  let title = document.createElement("h2");
+  title.className = "recent-post-box-title";
+  let subTitle = document.createElement("h3");
+  title.className = "recent-post-box-sub-title";
+  let summaryTitle = document.createElement("p");
+  summaryTitle.className = "recent-post-box-summary";
+
+  // Create text text
+  textBox.appendChild(title);
+  textBox.appendChild(subTitle);
+  textBox.appendChild(summaryTitle);
+
+  return textBox;
+}
+
 function getNewRecentPost() {
   /**
    *  Return new div for a recent post summary.
    *
    *  @return div containing recent post summary.
    */
-   const div = document.createElement("div");
-   let img = document.createElement("img");
-   img.src = "images/banner.jpg";
+  const div = document.createElement("div");
 
-   div.appendChild(img);
-   div.className = "recent-post-box";
-   return div;
+  // Set image data
+  let img = document.createElement("img");
+  img.src = "images/banner.jpg";
+  img.className = "recent-post-box-img"
+
+  div.appendChild(img);
+  div.appendChild(getNewRecentPostTextBox());
+  div.className = "recent-post-box";
+  return div;
 }
 
 function createHomeHighlights() {
+  /**
+   *  Create highlights on the home page.
+   */
   let summaries = 3;
   let sumDiv = document.getElementById('home-highlights');
 
@@ -42,6 +74,9 @@ function createHomeHighlights() {
 }
 
 function createRecentPosts() {
+  /**
+   *  Create highlights on the home page.
+   */
   let summaries = 3;
   let sumDiv = document.getElementById('home-recent-posts');
 
